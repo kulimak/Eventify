8b62af956404e0821d668b1fb6d998987562039f
Kulimák Máté <kulimakmate@turr.hu>
2025-03-14 10:22:46 +0100

    companyReg componens létrehozása

---
+++ Public/src/app/components/company-reg/company-reg.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-company-reg',
+  standalone: true,
+  imports: [],
+  templateUrl: './company-reg.component.html',
+  styleUrl: './company-reg.component.scss'
+})
+export class CompanyRegComponent {
+
+}
