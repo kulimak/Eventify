03ffd17f05cce60441ea267faf79fb4b5c4950e0
Kulimák Máté <kulimakmate@turr.hu>
2025-04-01 12:27:05 +0200

    KV-51 felhasználói profil módosítása gombok és dialogok elkészítése

--- Public/src/app/components/profile/profile.component.ts
+++ Public/src/app/components/profile/profile.component.ts
@@ -4,15 +4,29 @@ import { FormsModule } from '@angular/forms';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { PasswordModule } from 'primeng/password';
 import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [InputTextModule, FormsModule, FloatLabelModule, PasswordModule, ButtonModule],
+  imports: [InputTextModule, FormsModule, FloatLabelModule, PasswordModule, ButtonModule, DialogModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
   userValue: string | undefined;
   emailValue!: string;
+  visibleEmail: boolean = false;
+  visibleUser: boolean = false;
+  visiblePass: boolean = false;
+
+    showUserNameDialog() {
+        this.visibleUser = true;
+    }
+    showEmailDialog() {
+      this.visibleEmail = true;
+    }
+    showPasswdDialog() {
+      this.visiblePass = true;
+    }
 }
