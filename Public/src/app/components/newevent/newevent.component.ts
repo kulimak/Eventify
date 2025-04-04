0a4ac3f0eede3a317098557b24b7ee062ad39339
Kewkew00 <csanadikevin@turr.hu>
2025-04-04 10:33:36 +0200

    event letrehozas oldal elkezdése

--- Public/src/app/components/newevent/newevent.component.ts
+++ Public/src/app/components/newevent/newevent.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { CalendarModule } from 'primeng/calendar';
 
 @Component({
   selector: 'app-newevent',
   standalone: true,
-  imports: [],
+  imports: [  FormsModule, ButtonModule, CardModule, InputTextModule,InputTextareaModule, CalendarModule],
   templateUrl: './newevent.component.html',
   styleUrl: './newevent.component.scss'
 })
 export class NeweventComponent {
-
+  value: string | undefined;
+  date: Date[] | undefined;
 }
