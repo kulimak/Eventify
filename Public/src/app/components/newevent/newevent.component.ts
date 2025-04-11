201912cd6870f4bf9339fb5736b985d0ac451913
Kewkew00 <csanadikevin@turr.hu>
2025-04-11 09:28:47 +0200

    kategória választás hozzáasdása az esemény létrehozásnál

--- Public/src/app/components/newevent/newevent.component.ts
+++ Public/src/app/components/newevent/newevent.component.ts
@@ -5,15 +5,26 @@ import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { InputTextareaModule } from 'primeng/inputtextarea';
 import { CalendarModule } from 'primeng/calendar';
+import { SelectButtonModule } from 'primeng/selectbutton';
+
 
 @Component({
   selector: 'app-newevent',
   standalone: true,
-  imports: [  FormsModule, ButtonModule, CardModule, InputTextModule,InputTextareaModule, CalendarModule],
+  imports: [  FormsModule, ButtonModule, CardModule, InputTextModule,InputTextareaModule, CalendarModule, SelectButtonModule],
   templateUrl: './newevent.component.html',
-  styleUrl: './newevent.component.scss'
+  styleUrl: './newevent.component.scss',
 })
 export class NeweventComponent {
   value: string | undefined;
   date: Date[] | undefined;
+
+
+  Catvalue!: number;
+    
+  paymentOptions: any[] = [
+      { name: 'Teszt1', value: 1 },
+      { name: 'Teszt2', value: 2 },
+      { name: 'Tesz3', value: 3 }
+  ];
 }
