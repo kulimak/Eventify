ef44f764a8c71fff9ca01e29cf670250867d9800
Kewkew00 <csanadikevin@turr.hu>
2025-04-16 18:36:32 +0200

    Profil oldal elkészítése

--- Public/src/app/components/profile/profile.component.ts
+++ Public/src/app/components/profile/profile.component.ts
@@ -6,17 +6,22 @@ import { PasswordModule } from 'primeng/password';
 import { ButtonModule } from 'primeng/button';
 import { DialogModule } from 'primeng/dialog';
 import { FooterComponent } from '../footer/footer.component';
+import { RatingModule } from 'primeng/rating';
+import { InputTextareaModule } from 'primeng/inputtextarea';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [InputTextModule, FormsModule, FloatLabelModule, PasswordModule, ButtonModule, DialogModule, FooterComponent],
+  imports: [InputTextModule, FormsModule, FloatLabelModule, PasswordModule, ButtonModule, DialogModule, FooterComponent, RatingModule, InputTextareaModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
   userValue: string | undefined;
   emailValue!: string;
+  passwordValue!: string;
+  newPasswordValue!: string;
+  ratingValue: number = 5;
   visibleEmail: boolean = false;
   visibleUser: boolean = false;
   visiblePass: boolean = false;
