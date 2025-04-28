a7a44f89939ee1a734fb20b992a743b48e8c692c
Kewkew00 <csanadikevin@turr.hu>
2025-04-28 22:01:31 +0200

    Navbar igazodjon a jogokhoz(felhasználó,cég,admin)

--- Public/src/app/components/navbar/navbar.component.ts
+++ Public/src/app/components/navbar/navbar.component.ts
@@ -11,87 +11,119 @@ import { IconField } from 'primeng/iconfield';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [ButtonModule, DialogModule, CommonModule, FormsModule, MenubarModule],
+  imports: [
+    ButtonModule,
+    DialogModule,
+    CommonModule,
+    FormsModule,
+    MenubarModule,
+  ],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent  implements OnInit{
-  constructor(
-    private auth: AuthService
-  ){}
+export class NavbarComponent implements OnInit {
+  constructor(private auth: AuthService) {}
   visible: boolean = false;
 
   showDialog() {
-      this.visible = true;
+    this.visible = true;
   }
-  
 
   items: MenuItem[] | undefined;
 
-  ngOnInit(): void{
-    this.auth.isLoggedIn$.subscribe(res => {
-      this.setupMenu(res)
+  ngOnInit(): void {
+    this.auth.isLoggedIn$.subscribe((res) => {
+      this.setupMenu(res);
     });
   }
 
-  setupMenu(isLoggedIn:boolean){
+  setupMenu(isLoggedIn: boolean) {
     this.items = [
-      ...(isLoggedIn) ? [
-        ...(this.auth.isUser() || this.auth.isCompany()) ? [
-
-          //logged in routes
-          {
-            label: 'Főoldal', icon: 'pi pi-home', routerLink: '/fooldal'
-          },
-          {
-            label: 'Esemény létrehozása', icon: 'pi pi-calendar-plus', routerLink: '/newevent'
-          },
-
-          {
-            label: 'Saját események', icon: 'pi pi-calendar', routerLink: '/myevents'
-          },
-
-          {
-            label: 'Statisztika', icon: 'pi pi-file', routerLink: '/stats'
-          },
-          
-          {
-            label: 'Profil',icon: 'pi pi-user', routerLink: '/profile'
-          },
-
-          {
-            label:'Kijelentkezés', icon: 'pi pi-sign-out', command: ()=>this.auth.logout()
-          } 
-
-        ] : [
-          
-        ]
-
-      ] : [
-
-        // logged out routes
-          {
-            label: 'Belépés',icon: 'pi pi-sign-in',routerLink: '/login',     
-          },
-          {
-            label: 'Esemény', icon: 'pi pi-box', routerLink: '/fullscreenevent'
-          },
-          {
-            label: 'Admin', icon: 'pi pi-users', routerLink: '/adminpage'
-          },
-          {
-            label: 'Regisztráció',icon: 'pi pi-user-plus',routerLink: '/register/userReg'
-          },
-
-          {
-            label: 'Felhasználó Feltételek',icon: 'pi pi-file',command: () => this.showDialog()
-          },
-          
-          {
-            label: 'Vissza a főoldalra',icon: 'pi pi-home', routerLink: '/landingpage'
-          }
-      ]
+      ...(isLoggedIn
+        ? [
+            ...(this.auth.isAdmin()
+              ? [
+                  // Admin belépett
+                  {
+                    label: 'Felhasználók kezelése',
+                    icon: 'pi pi-users',
+                    routerLink: '/adminpage',
+                  },
+                  {
+                    label: 'Események kezelése',
+                    icon: 'pi pi-calendar',
+                    routerLink: '/admin/events',
+                  },
+                  {
+                    label: 'Statisztika',
+                    icon: 'pi pi-file',
+                    routerLink: '/stats',
+                  },
+                  {
+                    label: 'Kijelentkezés',
+                    icon: 'pi pi-sign-out',
+                    command: () => this.auth.logout(),
+                  },
+                ]
+              : this.auth.isUser() || this.auth.isCompany()
+              ? [
+                  // Normál felhasználó vagy cég
+                  {
+                    label: 'Főoldal',
+                    icon: 'pi pi-home',
+                    routerLink: '/fooldal',
+                  },
+                  {
+                    label: 'Esemény létrehozása',
+                    icon: 'pi pi-calendar-plus',
+                    routerLink: '/newevent',
+                  },
+                  {
+                    label: 'Saját események',
+                    icon: 'pi pi-calendar',
+                    routerLink: '/myevents',
+                  },
+                  {
+                    label: 'Esemény',
+                    icon: 'pi pi-box',
+                    routerLink: '/fullscreenevent',
+                  },
+                  {
+                    label: 'Profil',
+                    icon: 'pi pi-user',
+                    routerLink: '/profile',
+                  },
+                  {
+                    label: 'Kijelentkezés',
+                    icon: 'pi pi-sign-out',
+                    command: () => this.auth.logout(),
+                  },
+                ]
+              : []),
+          ]
+        : [
+            // Nem bejelentkezett user menü
+            {
+              label: 'Belépés',
+              icon: 'pi pi-sign-in',
+              routerLink: '/login',
+            },
+            {
+              label: 'Regisztráció',
+              icon: 'pi pi-user-plus',
+              routerLink: '/register/userReg',
+            },
+            {
+              label: 'Felhasználó Feltételek',
+              icon: 'pi pi-file',
+              command: () => this.showDialog(),
+            },
+            {
+              label: 'Vissza a főoldalra',
+              icon: 'pi pi-home',
+              routerLink: '/landingpage',
+            },
+          ]),
     ];
   }
-  
-}
+} 
